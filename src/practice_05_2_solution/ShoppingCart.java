5557c7fb0d05cb0deec2a7a56f6708d2fba5b0a5
Denis Blanari <dblanari@locomote.com>
2017-11-10 10:26:36 +0200

    Add practice 05 solutions

---
+++ src/practice_05_2_solution/ShoppingCart.java
@@ -0,0 +1,21 @@
+/* Copyright © 2016 Oracle and/or its affiliates. All rights reserved. */
+
+package practice_05_2_solution;
+
+public class ShoppingCart {
+    public static void main(String[] args){
+        String custName = "Mary Smith";
+        String message;
+ 
+        // Declare and initialize the items String array with 4 item descriptions.
+        String[] items = {"Shirt", "Socks", "Scarf", "Belt"};
+        
+        // Change message to show the number of items purchased.
+        message = custName +" wants to purchase " +items.length +" items.";
+        System.out.println(message);
+        
+        // Print an element from the items array.
+        System.out.println(items[2]);
+        //System.out.println(items[4]);
+    }
+}
