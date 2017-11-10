09c97a1547bdf5e7aa75098a723468defc5be3f2
Denis Blanari <dblanari@locomote.com>
2017-11-10 10:31:17 +0200

    Add practice 06

---
+++ src/practice_06_3/soccer/Goal.java
@@ -0,0 +1,9 @@
+/* Copyright © 2016 Oracle and/or its affiliates. All rights reserved. */
+
+package practice_06_3.soccer;
+
+public class Goal {   
+    public Team theTeam;
+    public Player thePlayer;
+    public double theTime;
+}
---
+++ src/practice_06_3/soccer/Player.java
@@ -0,0 +1,7 @@
+/* Copyright © 2016 Oracle and/or its affiliates. All rights reserved. */
+
+package practice_06_3.soccer;
+
+public class Player {
+    public String playerName;   
+}
