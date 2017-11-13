711859fd36f733712caccd008fef5d80aeddecf2
Denis Blanari <dblanari@locomote.com>
2017-11-13 14:06:37 +0200

    Add practice 07

---
+++ src/practice_07_4/soccer/Game.java
@@ -0,0 +1,9 @@
+/* Copyright © 2016 Oracle and/or its affiliates. All rights reserved. */
+
+package practice_07_4.soccer;
+
+public class Game {
+    public Team homeTeam;
+    public Team awayTeam;
+    public Goal[] goals;   
+}
---
+++ src/practice_07_4/soccer/soccer_solution/Team.java
@@ -0,0 +1,8 @@
+/* Copyright © 2016 Oracle and/or its affiliates. All rights reserved. */
+
+package practice_07_4.soccer.soccer_solution;
+
+public class Team {
+    public String teamName;
+    public Player[] playerArray;  
+}
