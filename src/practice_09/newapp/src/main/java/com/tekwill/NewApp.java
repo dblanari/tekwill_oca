f44f18a508e112a435d2624eeaa1363a839e44bf
dblanari <dblanari@endava.com>
2017-11-22 11:06:47 +0200

    Add new app

---
+++ src/practice_09/newapp/src/main/java/com/tekwill/NewApp.java
@@ -0,0 +1,10 @@
+package com.tekwill;
+
+public class NewApp {
+    public void display() {
+        System.out.println("========================================");
+        System.out.println("=============HELLO======================");
+        System.out.println("=============WORLD======================");
+        System.out.println("========================================");
+    }
+}
