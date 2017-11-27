2ceefd813ef995482768eef2750a99f39e605a44
dblanari <Denis.Blanari@endava.com>
2017-11-27 12:09:44 +0200

    Move project to maven module

---
+++ maven/src/main/java/com/endava/App.java
@@ -0,0 +1,17 @@
+package com.endava;
+
+import com.tekwill.NewApp;
+
+
+public class App {
+    public static void main(String[] args) {
+        NewApp app = new NewApp();
+        app.display();
+
+        System.out.println("Hello World");
+    }
+
+    public static boolean status() {
+        return true;
+    }
+}
