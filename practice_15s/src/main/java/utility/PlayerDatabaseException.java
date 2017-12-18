54b1d356a42aec70e86c896ef60e139fd6df8191
dblanari <denis.blanari@gmail.com>
2017-12-18 15:07:31 +0200

    add practice 15

---
+++ practice_15s/src/main/java/utility/PlayerDatabaseException.java
@@ -0,0 +1,9 @@
+/* Copyright © 2016 Oracle and/or its affiliates. All rights reserved. */
+package utility;
+
+public class PlayerDatabaseException extends Exception {
+    
+    public PlayerDatabaseException(String message){
+        super(message);
+    }
+}
