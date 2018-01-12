881e85b49f4948ba653d20b484a16c3c0534363d
Denis Blanari <denis.blanari@endava.com>
2018-01-12 08:54:00 +0200

    push to heroku

--- practice_16/src/main/java/com/example/rest/Application.java
+++ practice_16/src/main/java/com/example/rest/Application.java
@@ -1,24 +1,39 @@
 /* Copyright © 2016 Oracle and/or its affiliates. All rights reserved. */
 package com.example.rest;
 
-import java.util.Properties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+//@SpringBootApplication
+//public class Application {
+//
+//    public static final Properties myProps = new Properties();
+//
+//    public static void main(String[] args) {
+//        // Set properties
+//
+//        myProps.setProperty("server.address", "0.0.0.0");
+//        myProps.setProperty("server.port", "8080");
+//
+//        SpringApplication app = new SpringApplication(Application.class);
+//        app.setDefaultProperties(myProps);
+//        app.run(args);
+//
+//    }
+//}
+
 
 @SpringBootApplication
-public class Application {
-
-    public static final Properties myProps = new Properties();
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        // Set properties
+        SpringApplication.run(Application.class, args);
+    }
 
-        myProps.setProperty("server.address", "localhost");
-        myProps.setProperty("server.port", "8080");
-
-        SpringApplication app = new SpringApplication(Application.class);
-        app.setDefaultProperties(myProps);
-        app.run(args);
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
--- practice_16/src/main/java/com/example/rest/CustomerController.java
+++ practice_16/src/main/java/com/example/rest/CustomerController.java
@@ -2,16 +2,12 @@
 
 package com.example.rest;
 
-import java.util.concurrent.CopyOnWriteArrayList;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/customers")
@@ -25,23 +21,33 @@ public class CustomerController {
         return cList.toArray(new Customer[0]);
     }
 
+    // Get all customers under 20 years
+    @RequestMapping(value = "/under20", method = RequestMethod.GET)
+    public Customer[] getAllUnder20Years() {
+        return cList
+                .stream()
+                .filter(s -> (2018 - Integer.parseInt(s.getBirthday().substring(0, 4)) < 20))
+                .collect(Collectors.toList())
+                .toArray(new Customer[0]);
+    }
+
     // Get a customer
     @RequestMapping(method = RequestMethod.GET, value = "{id}")
     public Customer get(@PathVariable long id) {
-        
+
         Customer match = null;
-        
-        for(Customer c:cList){
-            if (c.getId() == id){
+
+        for (Customer c : cList) {
+            if (c.getId() == id) {
                 match = c;
-            }  
+            }
         }
-        
-        if (match != null){
+
+        if (match != null) {
             return match;
         } else {
-            throw new NotFoundException(id + " not found");            
-        }                
+            throw new NotFoundException(id + " not found");
+        }
 
     }
 
@@ -51,56 +57,56 @@ public class CustomerController {
         cList.add(customer);
         return new ResponseEntity<>(null, HttpStatus.CREATED);
     }
- 
-    
+
+
     // Update a Customer
     @RequestMapping(method = RequestMethod.PUT, value = "{id}")
     public ResponseEntity update(@PathVariable long id, @RequestBody Customer customer) {
-        
+
         int matchIndex = -1;
-        
-        for(Customer c:cList){
-            if (c.getId() == id){
+
+        for (Customer c : cList) {
+            if (c.getId() == id) {
                 matchIndex = cList.indexOf(c);
-            }  
+            }
         }
-        
-        if (matchIndex > -1){
+
+        if (matchIndex > -1) {
             cList.set(matchIndex, customer);
-            return new ResponseEntity<>(null, HttpStatus.OK);           
+            return new ResponseEntity<>(null, HttpStatus.OK);
         } else {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);           
-        }                
-   }
-   
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
 
     // Delete a Customer
     @RequestMapping(method = RequestMethod.DELETE, value = "{id}")
     public ResponseEntity delete(@PathVariable long id) {
-        
+
         int matchIndex = -1;
-        
-        for(Customer c:cList){
-            if (c.getId() == id){
+
+        for (Customer c : cList) {
+            if (c.getId() == id) {
                 matchIndex = cList.indexOf(c);
-            }  
+            }
         }
-        
-        if (matchIndex > -1){
+
+        if (matchIndex > -1) {
             cList.remove(matchIndex);
-            return new ResponseEntity<>(null, HttpStatus.OK);           
+            return new ResponseEntity<>(null, HttpStatus.OK);
         } else {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);           
-        }                
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
     }
-    
-    
+
+
     // Throw this exception when a lookup fails
     @ExceptionHandler(NotFoundException.class)
     @ResponseBody
     public ResponseEntity<?> myError(Exception exception) {
         return new ResponseEntity<>(new JsonError("ID not found error:", exception.getMessage()), HttpStatus.NOT_FOUND);
     }
-    
+
 
 }
