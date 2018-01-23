0cca92607242fcc949c54f86243702b615e92b1b
Denis Blanari <denis.blanari@endava.com>
2018-01-23 17:21:22 +0200

    add BasicOperations

---
+++ codewars/src/main/java/codewars/BasicOperations.java
@@ -0,0 +1,19 @@
+package codewars;
+
+/**
+ * Your task is to create a function - basic_op().
+ * <p>
+ * The function should take three arguments - operation(string/char), value1(number), value2(number). The function should return result of numbers after applying the chosen operation.
+ * <p>
+ * Examples:
+ * <p>
+ * basicOp('+', 4, 7)         // Output: 11
+ * basicOp('-', 15, 18)       // Output: -3
+ * basicOp('*', 5, 5)         // Output: 25
+ * basicOp('/', 49, 7)        // Output: 7
+ */
+public class BasicOperations {
+    public static Integer basicMath(String op, int v1, int v2) {
+        return 0;
+    }
+}
---
+++ codewars/src/test/java/BasicOperationsTestSuite.java
@@ -0,0 +1,10 @@
+import codewars.BasicOperationsTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        BasicOperationsTest.class
+})
+public class BasicOperationsTestSuite {
+}
