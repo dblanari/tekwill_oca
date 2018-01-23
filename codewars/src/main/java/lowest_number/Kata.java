c4ea3e98664b8bb9961593c439ea09150daefa76
Denis Blanari <denis.blanari@endava.com>
2018-01-23 14:55:13 +0200

    push lowers number

--- codewars/src/main/java/lowest_number/Kata.java
+++ codewars/src/main/java/lowest_number/Kata.java
@@ -1,7 +1,14 @@
 package lowest_number;
 
+import java.util.Arrays;
+
 public class Kata {
     public static int sum(int[] numbers) {
-        return 0;
+        Arrays.sort(numbers);
+        int sum = 0;
+        for (int i = 1; i < numbers.length - 1; i++) {
+            sum += numbers[i];
+        }
+        return sum;
     }
 }
