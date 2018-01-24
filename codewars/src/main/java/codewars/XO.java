7441d795d1353ff87282e9141b7a7c3cbe10cdaa
Denis Blanari <denis.blanari@endava.com>
2018-01-24 19:22:18 +0200

    Ho example

---
+++ codewars/src/main/java/codewars/XO.java
@@ -0,0 +1,20 @@
+package codewars;
+
+/**
+ * Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+ * <p>
+ * Examples input/output:
+ * <p>
+ * XO("ooxx") => true
+ * XO("xooxx") => false
+ * XO("ooxXm") => true
+ * XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+ * XO("zzoo") => false
+ * FUNDAMENTALS
+ */
+public class XO {
+    public static boolean getXO(String str) {
+        // Good Luck!!
+        return false;
+    }
+}
