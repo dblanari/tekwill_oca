b89838bc6052be45c8fa89611b3d298fb1cc1c24
Gerakles <bogdania567@gmail.com>
2018-01-24 19:01:34 +0200

    dutkf

--- codewars/src/main/java/codewars/BasicOperations.java
+++ codewars/src/main/java/codewars/BasicOperations.java
@@ -14,6 +14,19 @@ package codewars;
  */
 public class BasicOperations {
     public static Integer basicMath(String op, int v1, int v2) {
-        return 0;
+        int opresult = 0;
+        if (op == "+") {
+            opresult= v1 + v2;
+        }
+        if (op == "-") {
+            opresult= v1 - v2;
+        }
+        if (op == "/") {
+            opresult= v1 / v2;
+        }
+        if (op == "*") {
+            opresult= v1 * v2;
+        } ;
+    return opresult;
     }
 }
