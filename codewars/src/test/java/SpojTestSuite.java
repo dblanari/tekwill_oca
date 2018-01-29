1de91536de0d85f6b29018ce19c3dd770164c881
Denis Blanari <denis.blanari@endava.com>
2018-01-29 16:51:46 +0200

    add spoj test suite

---
+++ codewars/src/test/java/SpojTestSuite.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import spoj.ex1.Main1Test;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        Main1Test.class
+})
+public class SpojTestSuite {
+}
