366919289df3b25c34f90b3b280ca800e3861048
CorpLexx <corplexx@gmail.com>
2018-01-29 18:44:52 +0200

    comm

--- codewars/src/main/java/spoj/ex1/Main2.java
+++ codewars/src/main/java/spoj/ex1/Main2.java
@@ -1,6 +1,14 @@
 package spoj.ex1;
 
+import java.util.Scanner;
+
 class Main2 {
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int i;
+        while(in.hasNext() && (i = in.nextInt()) != 42) {
+            System.out.println(i);
+        }
     }
 }
