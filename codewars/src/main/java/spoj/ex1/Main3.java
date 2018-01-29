c99cc5d5df66a3d3de6d7265669bf29d6dc47c94
Gerakles <bogdania567@gmail.com>
2018-01-29 18:54:59 +0200

    Wx 1

--- codewars/src/main/java/spoj/ex1/Main3.java
+++ codewars/src/main/java/spoj/ex1/Main3.java
@@ -1,6 +1,16 @@
 package spoj.ex1;
 
+import java.util.Scanner;
+
 class Main3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner( System.in );
+        int y;
+        while (scanner.hasNext()) {
+            y = scanner.nextInt();
+            if (y == 42)
+                break;
+            System.out.println(y);
+        }
     }
 }
--- codewars/src/test/java/spoj/ex1/Main3Test.java
+++ codewars/src/test/java/spoj/ex1/Main3Test.java
@@ -3,6 +3,6 @@ package spoj.ex1;
 public class Main3Test extends MainAbstract {
 
     protected void callMain() {
-        Main2.main(null);
+        Main3.main(null);
     }
 }
